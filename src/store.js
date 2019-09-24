7e500a296ed187b7aa126aebcb46e187d151a774
Kennedy Njeri <mistakenz123@gmail.com>
2019-09-24 16:45:59 +0300

    fix balance

--- src/store.js
+++ src/store.js
@@ -15,13 +15,11 @@ const firebaseConfig = {
 
 }
 
-
 // react-redux-firebase config
 const rrfConfig = {
     userProfile: 'users',
     useFirestoreForProfile: true // Firestore for Profile instead of Realtime DB
 }
-
 
 // Initialize firebase instance
 firebase.initializeApp(firebaseConfig)
